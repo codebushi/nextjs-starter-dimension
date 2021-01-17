edd644b6cedecdc20edc54b6fa7fe3b79b8eb7f5
krav maguy <flex4lease@gmail.com>
2021-01-16 19:39:18 -0600

    added favicon

--- components/MailForm.js
+++ components/MailForm.js
@@ -1,45 +1,67 @@
-import { useState } from "react";
-const MailForm = () => {
-  const [inputs, setInputs] = useState({});
-  const handleSubmit = (event) => {
-    if (event) {
-      event.preventDefault();
-    }
-    callback();
-  };
+// import { useState, useEffect, useRef } from "react";
+class MailForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  handleChange=(e)=>{
+    e.preventDefault
+    const name=e.target.name
+    const val=e.target.value
+    console.log(name)
+    console.log(val)
 
-  const handleInputChange = (event) => {
-    event.persist();
-    setInputs((inputs) => ({
-      ...inputs,
-      [event.target.name]: event.target.value,
-    }));
-  };
-
-  return (
-    <form method="post" action="#">
-      <div className="field half first">
-        <label htmlFor="name">Name</label>
-        <input onChange={handleInputChange} type="text" name="name" id="name" required />
-      </div>
-      <div className="field half">
-        <label htmlFor="email">Email</label>
-        <input onChange={handleInputChange} type="text" name="email" id="email" required />
-      </div>
-      <div className="field">
-        <label htmlFor="message">Message</label>
-        <textarea onChange={handleInputChange} name="message" id="message" rows="4" required></textarea>
-      </div>
-      <ul className="actions">
-        <li>
-          <input onClick={handleSubmit} type="submit" value="Send Message" className="special" />
-        </li>
-        <li>
-          <input type="reset" value="Reset" />
-        </li>
-      </ul>
-    </form>
-  );
-};
-
+  }
+  render() {
+    return (
+      <form method="post" action="#">
+        <div className="field half first">
+          <div id="name-error"></div>
+          <label htmlFor="name">Name</label>
+          <input
+            // ref={}
+            onChange={this.handleChange}
+            type="text"
+            name="name"
+            id="name"
+            required
+          />
+        </div>
+        <div className="field half">
+          <label htmlFor="email">Email</label>
+          <input
+            onChange={this.handleChange}
+            type="text"
+            name="email"
+            id="email"
+            required
+          />
+        </div>
+        <div className="field">
+          <label htmlFor="message">Message</label>
+          <textarea
+            onChange={this.handleChange}
+            name="message"
+            id="message"
+            rows="4"
+            required
+          ></textarea>
+        </div>
+        <ul className="actions">
+          <li>
+            <input
+              onClick={null}
+              type="submit"
+              value="Send Message"
+              className="special"
+            />
+          </li>
+          <li>
+            <input type="reset" value="Reset" />
+          </li>
+        </ul>
+      </form>
+    );
+  }
+}
 export default MailForm;
