54b1b6f1ce4de0699f4a6b5b49c2d4e5f77433e5
krav maguy <flex4lease@gmail.com>
2021-01-18 23:47:15 -0600

    FaViconfile

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,8 @@ import Header from "../components/Header";
 import Main from "../components/Main";
 import Footer from "../components/Footer";
 import ProfileImage from "../components/Profile-Image"
-
+import { FaVoicemail } from "react-icons/fa";
+import FaViconfile from "../components/FaViconfile"
 // import "../static/css/avatar.css";
 
 class IndexPage extends React.Component {
@@ -104,82 +105,7 @@ class IndexPage extends React.Component {
               rel="stylesheet"
             />
 
-            <link
-              rel="apple-touch-icon"
-              sizes="57x57"
-              href="/static/favicon-profile/apple-icon-57x57.png"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="60x60"
-              href="/static/favicon-profile/apple-icon-60x60.png"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="72x72"
-              href="/static/favicon-profile/apple-icon-72x72.png"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="76x76"
-              href="/static/favicon-profile/apple-icon-76x76.png"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="114x114"
-              href="/static/favicon-profile/apple-icon-114x114.png"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="120x120"
-              href="/static/favicon-profile/apple-icon-120x120.png"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="144x144"
-              href="/static/favicon-profile/apple-icon-144x144.png"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="152x152"
-              href="/static/favicon-profile/apple-icon-152x152.png"
-            />
-            <link
-              rel="apple-touch-icon"
-              sizes="180x180"
-              href="/static/favicon-profile/apple-icon-180x180.png"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              sizes="192x192"
-              href="/static/favicon-profile/android-icon-192x192.png"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              sizes="32x32"
-              href="/static/favicon-profile/favicon-32x32.png"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              sizes="96x96"
-              href="/static/favicon-profile/favicon-96x96.png"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              sizes="16x16"
-              href="/static/favicon-profile/favicon-16x16.png"
-            />
-            <link rel="manifest" href="/static/favicon-profile/manifest.json" />
-            <meta name="msapplication-TileColor" content="#ffffff" />
-            <meta
-              name="msapplication-TileImage"
-              content="/static/favicon-profile/ms-icon-144x144.png"
-            />
-            <meta name="theme-color" content="#ffffff" />
+         <FaViconfile/>
           </Head>
 
           <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
